1623f712e45834b5a8f61f222e9d7b4f9b984e4b
Ronnakrit294 <71628250+Ronnakrit294@users.noreply.github.com>
2020-10-29 16:21:20 +0700

    Update linebot_basic.php

--- linebot_basic.php
+++ linebot_basic.php
@@ -75,7 +75,7 @@ function mySQL_selectAll($url)
 	$data = "ผลลัพธ์:\r\n";
 		
 	foreach($result_json as $values) {
-		$data .= $values["user_stuid"] . " " . $values["user_firstname"] . " " . $values["user_lastname"] . "\r\n";
+		$data .= $values["stuid"] . " " . $values["fullname"] . "\r\n";
 	}
 	
 	return $data;
